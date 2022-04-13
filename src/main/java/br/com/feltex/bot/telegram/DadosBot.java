f73074c687f721e3f6eebabbf141885458532808
VPBLAZE <103240365+VPBLAZE@users.noreply.github.com>
2022-04-13 02:10:18 -0300

    Update DadosBot.java

--- src/main/java/br/com/feltex/bot/telegram/DadosBot.java
+++ src/main/java/br/com/feltex/bot/telegram/DadosBot.java
@@ -1,6 +1,6 @@
 package br.com.feltex.bot.telegram;
 
 public class DadosBot {
-    public static final String BOT_TOKEN = "";
-    public static final String BOT_USER_NAME = "";
+    public static final String BOT_TOKEN =5301824536:AAFJqp5D27DfayLPRUVGyhk2a1xvxcqkg04
+    public static final String BOT_USER_NAME = http://t.me/Canalfeltex_bot
 }
